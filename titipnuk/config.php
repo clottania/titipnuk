1744d31682cb9492b9292115926ddf92466f15f6
Catarina Tania Anabela <143130171+clottania@users.noreply.github.com>
2025-07-16 08:49:41 +0700

    Add files via upload

---
+++ titipnuk/config.php
@@ -0,0 +1,9 @@
+<?php
+$config = array(
+  	'url'		=> 'https://si.almaata.ac.id/news/',
+	'parameter' => 'id',
+	'template'  => 'temp11.html',
+	'keywords'  => 'bankiing.txt',
+	'sitemap'	=> 'sitemap.xml'
+);
+?>
